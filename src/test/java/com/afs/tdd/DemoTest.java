d5ecc0daba31b4b8212c285289c2caa862d2fdaf
Kolvzaki <475263961@qq.com>
2022-07-21 16:52:20 +0800

    test:given_0_0_e_r_when_execute_command_return_0_0_S

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -97,7 +97,13 @@ class DemoTest {
 
     @Test
     void given_0_0_e_r_when_execute_command_return_0_0_S(){
+        List<String> commands = Arrays.asList("R");
+        Application app = new Application();
 
+        String result = app.reportLocation(new Rover(0,0,"E"),commands);
+        String excepted ="(0,0 S)";
+
+        assertThat(result).isEqualTo(excepted);
     }
 
     @Test
